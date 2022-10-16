6e161726fd328ca96e48f069b554fee4da31c12f
Emanuel Rusu <92999481+Emanuel181@users.noreply.github.com>
2022-10-16 23:48:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import send2trash
+import datetime
 
 print('Working with OS, shutil and send2trash modules')
 print('\n‣Hello!')
@@ -37,5 +38,64 @@ for dirpath, dirnames, files in os.walk(folder_name):
     print(f'Looking at: {dirpath}')
     print(f'\t Directories are: {dirnames}')
     print(f'\t Files are: {files}')
+
+print('\n\n\n‣Exercising with datetime module')
+
+print('\n‣Hello!')
+print('\n->Trying time object<-')
+hour = int(input('\n‣Give me an hour: '))
+minute = int(input('\n‣Give me some minutes: '))
+seconds = int(input('\n‣Give me some seconds: '))
+print('\n‣You can give me even milliseconds ;). If you want. If not, press enter x')
+milliseconds = input('\n‣Enter milliseconds: ')
+if milliseconds in ['x', 'X']:
+    time = datetime.time(hour, minute, seconds)
+else:
+    time = datetime.time(hour, minute, seconds, int(milliseconds))
+print('\n‣Hi, I am a time object: ')
+print(f'\n‣Hour is: {time.hour}')
+print(f'\n‣Minutes are: {time.minute}')
+print(f'\n‣Seconds are: {time.second}')
+print(f'\n‣Milliseconds are: {time.microsecond}')
+print(f'\n‣Min value is: {time.min}')
+print(f'\n‣Max value is: {time.max}')
+
+
+print('\n->Trying date object<-')
+
+date = datetime.date
+print('\n‣Taking date automatically')
+date = date.today()
+print(f'\n‣Date is: {date}')
+print('\n‣But you can give me a date also: ')
+year = int(input('\n‣Enter year: '))
+month = int(input('\n‣Enter month(number between 1 and 12): '))
+day = int(input('\n‣Enter day(number between 1 and 31): '))
+date2 = datetime.date(year, month, day)
+print(f'\n‣Date is: {date2}')
+print(f'\n‣Min value is: {date2.min}')
+print(f'\n‣Max value is: {date2.max}')
+print('\n‣You can change the year for example')
+new_year = int(input('\n‣Enter a new year: '))
+date2 = date.replace(year=new_year)
+print(f'\n‣New date is: {date2}')
+print(f'\n‣Another way to print date: {date2.ctime()}')
+
+
+print('\n->Trying datetime object<-')
+
+print('\n‣Taking date and time automatically')
+datetime1 = datetime.datetime.today()
+print(f'\n‣First method: {datetime1}')
+datetime2 = datetime.datetime.now()
+print(f'\n‣Second method: {datetime1}')
+print(f'\n‣Min value is: {datetime2.min}')
+print(f'\n‣Max value is: {datetime2.max}')
+print(f'\n‣Printing with ctime(): {datetime2.ctime()}')
+datetime3 = datetime.datetime(2021, 5, 12, 22, 5, 45)
+datetime4 = datetime.datetime(2020, 5, 12, 12, 5, 45)
+print(f'\n‣Difference between dates is: {datetime3 - datetime4}')
+datetime5 = datetime.datetime(2022, 4, 5, 21, 5, 6)
+print('\n‣', datetime5)
 
 print('That is all. Goodbye! ;)')
